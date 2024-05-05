b68f7f6b5f7e2e58c916ec18fc7670c5e4659760
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-05-05 08:49:34 +0700

    create jsp file

---
+++ src/main/java/com/ntkhoa/dashboard/Controller/Login.java
@@ -0,0 +1,23 @@
+package com.ntkhoa.dashboard.Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "Login", value = "/Login")
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
